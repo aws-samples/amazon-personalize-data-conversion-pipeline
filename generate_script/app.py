011e2036092808028d24b9a751a8285406969a46
Scott Hosfield <horsfieldsa@gmail.com>
2019-01-16 15:50:57 -0800

    Moves template script to external file

--- generate_script/app.py
+++ generate_script/app.py
@@ -7,27 +7,15 @@ def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
 
-    script_template = """import sys
-    from awsglue.transforms import *
-    from awsglue.utils import getResolvedOptions
-    from pyspark.context import SparkContext
-    from awsglue.context import GlueContext
-    from awsglue.job import Job
-    from awsglue.dynamicframe import DynamicFrame
-    args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-    sc = SparkContext()
-    glueContext = GlueContext(sc)
-    spark = glueContext.spark_session
-    job = Job(glueContext)
-    job.init(args['JOB_NAME'], args)
-    datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "[DATABASE_NAME]", table_name = "[TABLE_NAME]", transformation_ctx = "datasource0")
-    applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("[S_ITEM_ID]", "int", "[D_ITEM_ID]", "int"), ("[S_USER_ID]", "int", "[D_USER_ID]", "int"), ("[S_EVENT_TYPE]", "string", "[D_EVENT_TYPE]", "string"), ("[S_EVENT_VAL]", "int", "[D_EVENT_VAL]", "int"), ("[S_TIME]", "int", "[D_TIME]", "int"), transformation_ctx = "applymapping1")
-    repartitioned_df = applymapping1.toDF().repartition(1)
-    repartitioned_dynf = DynamicFrame.fromDF(repartitioned_df, glueContext, "nested")
-    datasink2 = glueContext.write_dynamic_frame.from_options(frame = repartitioned_dynf, connection_type = "s3", connection_options = {"path": "[OUTPUT_PATH]"}, format = "csv", transformation_ctx = "datasink2")
-    job.commit()
-    """
+    # Script Template for Conversion Glue Job
+    script_template = ""
 
+    # Read Script Template
+    script_template_file = os.environ['LAMBDA_TASK_ROOT'] + '/glueJobScript.template'
+    with open(script_template_file, 'r') as template_file:
+        script_template=template_file.read()
+
+    # Replace Placeholder Values with ResourceProperties
     script_template = script_template.replace('[S_ITEM_ID]', event['ResourceProperties']['SourceColumnItemId'])
     script_template = script_template.replace('[D_ITEM_ID]', event['ResourceProperties']['DestinationColumnItemId'])
     script_template = script_template.replace('[S_USER_ID]', event['ResourceProperties']['SourceColumnUserId'])
@@ -44,8 +32,6 @@ def lambda_handler(event, context):
 
     script_bucket = os.environ['CONVERSION_JOB_SCRIPT_BUCKET']
     script_filename = 'conversionScript'
-
-    print(script_template)
 
     try:
         if event['RequestType'] == 'Create':
